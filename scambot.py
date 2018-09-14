a34967f04fec36ba343e77f58f258ddcb30d6061
Aleksandr <asdiamond@miners.utep.edu>
2018-09-14 15:55:25 -0600

    Added a simple wordpair to demonstrate how to use nltk Chatbot library.

--- scambot.py
+++ scambot.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-import platform, re, nltk
+# A tech support scammer bot
+# Authors: asdiamond, eseymour
+# for liscence information see LICENSE
+# a translation table used to convert
+# things you say into things the computer
+# says back, e.g. "I am" -> "you are"
+#
+# Inspired by the chatbot demos in nltk
 
+import platform, re, nltk
 
 from nltk.chat.util import Chat, reflections
 
@@ -12,13 +20,33 @@ node = platform.node()
 dist = platform.dist() if system == 'Linux' else None
 distFull = f"{dist[0]} {dist[2]}" if dist else None
 
-print(f"I'm {{name}} calling from the {system} help desk. We have detected \
-errors in your {processor} {architecture} processor's microcode. We need to \
-update your {node} {distFull} machine to the latest microcode to ...")
+# a table of response pairs, where each pair consists of a
+# regular expression, and a list of possible responses,
+# with group-macros labelled as %1, %2.
+
+name = "bobert"
+
+pairs = (
+
+    (r'Who is this',
+     (f"I'm {name} calling from the {system} help desk.",
+      f"\nHello, I'm {name} from the {system} help desk. We have"
+      f"\ndetected errors in your {processor} {architecture} processor's"
+      f"\nmicrocode. We need to update your {node} {distFull} machine " # FIXME distFull is None on windows.
+      f"\nto the latest microcode to ensure your security",
+      f"\nThis is the {system} help desk.")
+     ),
+
+)
+
+
+tech_support = Chat(pairs, reflections)
 
 
 def main():
-    nltk.chat.eliza.demo()
+    print("Tech support\n-----------")
+    print("Enter quit when done.")
+    tech_support.converse()
 
 
 if __name__ == '__main__':
